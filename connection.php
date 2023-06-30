fe20e47977e2139c5e50b9fc0e12a4b9ec32501e
Frank Chaparro <franklin25.2001@gmail.com>
2023-06-30 10:52:50 -0500

    nuevo añadido hasta cliente

--- connection.php
+++ connection.php
@@ -7,29 +7,5 @@
         'bembos',
     );
 
- /*
-    $query =   "SELECT * FROM categoria_producto;";
-
-    $result = mysqli_query($connection,$query);
-
-    if(!$result){
-      die('query error '.mysqli_error($connection));
-    }
-    
-    
-    $json = array();
-    while($row = mysqli_fetch_array($result)) {
-      $json[] = array(
-        'idcategoria_producto' => $row['idcategoria_producto'],
-        'nombre_categoria' => $row['nombre_categoria'],
-
-      );
-    }
-    $jsonstring = json_encode($json);
-    
-    echo $jsonstring;
-
-
-    */
-    
+ 
 ?>
---
+++ models/cliente.php
@@ -0,0 +1,25 @@
+<?php
+
+    include('../connection.php'); 
+
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }else{
+        echo "the database is connected";
+    }
+
+    //$nombreCliente = $_POST['nombreCliente'];
+    //$documento = $_POST['documento'];
+
+    //echo $nombreCliente;
+    //echo $documento;
+
+    $query = "INSERT INTO cliente(nombre_completo,documento) values ('frank aldair','1234563');";
+
+    if (mysqli_query($connection, $query)) {
+        echo "New client created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+    }
+
+?>
